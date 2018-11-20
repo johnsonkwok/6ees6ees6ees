12811891868ca9172300a6a63736e697523d6ec7
Johnson Kwok <johnsonkwok1@gmail.com>
2018-11-20 00:08:55 -0800

    Implement HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor(age, job, color, food, eat, honeyPot) {
+    super(color, food, eat);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+
+  makeHoney() {
+    this.honeyPot++;
+  }
+
+  giveHoney() {
+    this.honeyPot--;
+  }
 };
