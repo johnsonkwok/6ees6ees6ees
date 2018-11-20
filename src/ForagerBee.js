e7bf0fa192c6ad9614234dfa1f1306623354d90f
Johnson Kwok <johnsonkwok1@gmail.com>
2018-11-20 00:12:02 -0800

    Implement ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor(age, job, color, food, eat, canFly, treasureChest) {
+    super(color, food, eat);
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
